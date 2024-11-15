9751de0720901127bf57ab1b3074076a2af99c79
edircoli <edirvidalcastro@gmail.com>
2024-11-15 12:54:05 +0100

    Added a new transformation to DataPreprocessing

--- BatchEffectPlots.py
+++ BatchEffectPlots.py
@@ -8,19 +8,37 @@ import plotly.graph_objects as go
 from plotly.validators.scatter.marker import SymbolValidator
 from clustergrammer2 import net, Network, CGM2
 
-def DataPreprocess(path, preprocess = True, factors = ['sample', 'batch', 'tissue']):
+def DataPreprocess(path, preprocess = True, factors = ['sample', 'batch', 'tissue'], transformation = "CLR"):
     df = pd.read_csv(path)
     df[factors] = df[factors].astype('category')
 
     if (preprocess):
-        # Select only OTUs columns and adding a small offset
-        df_otu = df.select_dtypes(include='number') + 0.01
+        if transformation == "CLR":
+            # Select only OTUs columns and adding a small offset
+            df_otu = df.select_dtypes(include='number') + 0.01
 
-        # Apply CLR transformation to numeric columns
-        df_clr = np.log(df_otu.div(gmean(df_otu, axis=1), axis=0))
+            # Apply CLR transformation to numeric columns
+            df_clr = np.log(df_otu.div(gmean(df_otu, axis=1), axis=0))
 
-        # Combine CLR-transformed data with non-numeric columns
-        df = pd.concat([df[factors], df_clr], axis=1)
+            # Combine CLR-transformed data with non-numeric columns
+            df = pd.concat([df[factors], df_clr], axis=1)
+        
+        elif transformation == "Sqrt":
+            # Select only OTUs columns
+            df_out = df.select_dtypes(include='number')
+
+            #Apply Square-root transformation to numeric columns
+            df_sqrt = np.sqrt(df_otu)
+
+            #Standardize the squared-rooted data
+            scaler = StandardScaler()
+            df_stsqrt = scaler.fit_transform(df_sqrt)
+
+            #Convert back to DataFrame
+            df_stsqrt = pd.DataFrame(df_stsqrt, columns=df_sqrt.columns)
+
+            #Combine data with non-numeric columns
+            df = pd.concat([df[factors], df_stsqrt])
 
     return df
 
