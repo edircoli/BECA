4be898906fdfd7145ca1c15c9cbc1e4b3c5c948d
edircoli <edirvidalcastro@gmail.com>
2024-11-22 15:26:01 +0100

    Implemented iLISI metric

--- BatchEffectMetrics.py
+++ BatchEffectMetrics.py
@@ -37,3 +37,33 @@ def kBET(data, batch_label = "batch"):
         p_values.append(p_j)
 
     return sum(1 for p in p_values if p > 0.05) / n
+
+def iLISI(data, batch_label = "batch"):
+
+    data_otus = data.select_dtypes(include = "number")
+    data_batch = data[batch_label]
+
+    n = len(data_batch)
+    k = int(np.sqrt(data_otus.shape[0])) #k-nearest neighbors
+
+    knn = NearestNeighbors(n_neighbors = k, metric = "euclidean")
+    knn.fit(data_otus)
+
+    _, indx = knn.kneighbors(data_otus)
+
+    ISI_j = []
+    for neighborhood in indx:
+        p_j = []
+        for i in data_batch.unique():
+            p_ij = 0
+            for s in neighborhood:
+                if data_batch.iloc[s] == i:
+                    p_ij += 1 #Identifies batch label on neighborhood
+            p_ij = p_ij / len(neighborhood) #Proportion of batch label in neighborhood
+            p_j.append(p_ij**2)
+
+        ISI_j.append(1 / sum(p_j)) #Inverse Simpson's index for neighborhood
+
+    iLISI = 1/len(indx) * sum(ISI_j)
+    
+    return iLISI
