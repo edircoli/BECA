5b9e8edb081e26bd71f1aa36499286b53bb9cd6a
edircoli <edirvidalcastro@gmail.com>
2024-11-22 15:29:06 +0100

    Implemented cLISI metric

--- BatchEffectMetrics.py
+++ BatchEffectMetrics.py
@@ -67,3 +67,33 @@ def iLISI(data, batch_label = "batch"):
     iLISI = 1/len(indx) * sum(ISI_j)
     
     return iLISI
+
+def cLISI(data, cell_label = "tissue"):
+
+    data_otus = data.select_dtypes(include = "number")
+    data_cell = data[cell_label]
+
+    n = len(data_cell)
+    k = int(np.sqrt(data_otus.shape[0])) #k-nearest neighbors
+
+    knn = NearestNeighbors(n_neighbors = k, metric = "euclidean")
+    knn.fit(data_otus)
+
+    _, indx = knn.kneighbors(data_otus)
+
+    ISI_j = []
+    for neighborhood in indx:
+        p_j = []
+        for i in data_cell.unique():
+            p_ij = 0
+            for s in neighborhood:
+                if data_cell.iloc[s] == i:
+                    p_ij += 1 #Identifies batch label on neighborhood
+            p_ij = p_ij / len(neighborhood) #Proportion of batch label in neighborhood
+            p_j.append(p_ij**2)
+
+        ISI_j.append(1 / sum(p_j)) #Inverse Simpson's index for neighborhood
+
+    cLISI = 1/len(indx) * sum(ISI_j)
+    
+    return cLISI
