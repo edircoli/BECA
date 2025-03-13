cc7bc08a6e63e78fddb95de187f101ba6d9d147f
edircoli <edirvidalcastro@gmail.com>
2025-03-13 08:42:29 +0100

    Included BiologicalClassifiers

--- MetaABaCo.py
+++ MetaABaCo.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy.stats import gaussian_kde
 import matplotlib.pyplot as plt
 import math
+from BatchEffectDataLoader import class_to_int
 
 # ---------- PRIOR CLASSES DEFINITIONS ---------- #
 
@@ -55,6 +56,45 @@ class MoGPrior(nn.Module):
         self.n_comp = n_comp
 
         self.mu = nn.Parameter(torch.randn(n_comp, self.d_z) * multiplier)
+        self.var = nn.Parameter(torch.randn(n_comp, self.d_z))
+        self.pi = nn.Parameter(torch.zeros(n_comp))
+
+    def forward(self):
+        """
+        Return prior distribution, allowing for the computation of the KL-divergence by calling self.prior().
+
+        Returns:
+            prior: [torch.distributions.Distribution]
+        """
+        # Get parameters for each MoG component
+        means = self.mu
+        stds = torch.sqrt(torch.nn.functional.softplus(self.var) + 1e-8)
+        logits = self.pi
+
+        # Call MoG distribution
+        prior = MixtureOfGaussians(logits, means, stds)
+
+        return prior
+
+
+class VMMPrior(nn.Module):
+    def __init__(self, d_z, n_comp, mu, multiplier=1.0):
+        """
+        Define a Mixture of Gaussian Normal prior distribution.
+
+        Parameters:
+            d_z: [int]
+                Dimension of the latent space
+            n_comp: [int]
+                Number of components for the MoG distribution
+            multiplier: [float]
+                Parameter that controls sparsity of each Gaussian component
+        """
+        super().__init__()
+        self.d_z = d_z
+        self.n_comp = n_comp
+
+        self.mu = mu
         self.var = nn.Parameter(torch.randn(n_comp, self.d_z))
         self.pi = nn.Parameter(torch.zeros(n_comp))
 
@@ -725,7 +765,14 @@ class VampPriorVAE(nn.Module):
     """
 
     def __init__(
-        self, encoder, decoder, input_dim, K_pseudo_inputs, d_z, data_loader=None
+        self,
+        encoder,
+        decoder,
+        input_dim,
+        K_pseudo_inputs,
+        d_z,
+        beta=1.0,
+        data_loader=None,
     ):
         super().__init__()
         self.encoder = encoder
@@ -733,6 +780,7 @@ class VampPriorVAE(nn.Module):
         self.input_dim = input_dim
         self.K = K_pseudo_inputs
         self.d_z = d_z
+        self.beta = beta
 
         # Pseudo-inputs
         if data_loader is not None:
@@ -741,7 +789,7 @@ class VampPriorVAE(nn.Module):
             self.u = nn.Parameter(torch.rand(K_pseudo_inputs, input_dim))
 
         # Mixing weights
-        self.w = nn.Parameter(torch.zeros(K_pseudo_inputs, 1, 1))
+        self.w = nn.Parameter(torch.zeros(K_pseudo_inputs))
 
     def sample_from_dataloader(self, data_loader):
         all_data = []
@@ -754,32 +802,17 @@ class VampPriorVAE(nn.Module):
         all_data = torch.cat(all_data, dim=0)[: self.K]
         return nn.Parameter(all_data.clone().detach().requires_grad_(True))
 
-    def sample(self, batch_size):
+    def sample(self, n_samples):
         # encode pseudo inputs
-        mu_vp, logvar_vp = self.encoder.encode(self.u)
+        mu_vp, var_vp = self.encoder.encode(self.u)
+        std_vp = torch.sqrt(torch.nn.functional.softplus(var_vp) + 1e-8)
 
         # mixing probabilities
-        w = F.softmax(self.w, dim=0)
-        w = w.squeeze()
+        w = self.w.view(-1)
 
-        # pick components
-        indexes = torch.multinomial(w, batch_size, replacement=True)
+        mog = MixtureOfGaussians(w, mu_vp, std_vp)
 
-        # means and vars
-        eps = torch.randn(batch_size, self.d_z, device="cuda:0")
-
-        # for each point to be sampled
-        for i in range(batch_size):
-            indx = indexes[i]
-            if i == 0:
-                z = mu_vp[[indx]] + eps[[i]] * torch.exp(0.5 * logvar_vp[[indx]])
-            else:
-                z = torch.cat(
-                    (z, mu_vp[[indx]] + eps[[i]] * torch.exp(0.5 * logvar_vp[[indx]])),
-                    0,
-                )
-
-        return z
+        return mog.rsample(sample_shape=torch.Size([n_samples]))
 
     def pca_prior(self, n_samples):
         """
@@ -814,21 +847,16 @@ class VampPriorVAE(nn.Module):
         return pca.fit_transform(z.detach().cpu())
 
     def log_prob(self, z):
-        mu_vp, logvar_vp = self.encoder.encode(self.u)
+        mu_vp, var_vp = self.encoder.encode(self.u)
+        std_vp = torch.sqrt(torch.nn.functional.softplus(var_vp) + 1e-8)
 
         # mixing probabilities
-        w = F.softmax(self.w, dim=0)
+        w = self.w.view(-1)
 
-        # log-mog
-        z = z.unsqueeze(0)
-        mu_vp = mu_vp.unsqueeze(1)
-        logvar_vp = logvar_vp.unsqueeze(1)
+        # mog definition (VampPrior has the same form as the MoG class)
+        mog = MixtureOfGaussians(w, mu_vp, std_vp)
 
-        # log prob
-        log_p = log_normal_diag(z, mu_vp, logvar_vp, dim=-1) + torch.log(w)
-        log_prob = torch.logsumexp(log_p, dim=0)
-
-        return log_prob
+        return mog.log_prob(z)
 
     def forward(self, x):
         # Obtain posterior and sample
@@ -843,7 +871,142 @@ class VampPriorVAE(nn.Module):
         log_p_z = self.log_prob(z)
 
         recon_term = p_xz.log_prob(x).mean()
-        kl_term = (log_q_zx - log_p_z).mean()
+        kl_term = self.beta * (log_q_zx - log_p_z).mean()
+
+        return -recon_term + kl_term
+
+
+class VampPriorMixtureVAE(nn.Module):
+    """
+    Define a VampPrior Variational Autoencoder model.
+    """
+
+    def __init__(
+        self,
+        encoder,
+        decoder,
+        input_dim,
+        n_comps,
+        d_z,
+        beta=1.0,
+        data_loader=None,
+    ):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.input_dim = input_dim
+        self.K = n_comps
+        self.d_z = d_z
+        self.beta = beta
+
+        # Pseudo-inputs
+        if data_loader is not None:
+            self.u = self.sample_from_dataloader(data_loader)
+        else:
+            self.u = nn.Parameter(torch.rand(n_comps, input_dim))
+
+        # MoG prior parameters besides location
+        self.prior_var = nn.Parameter(torch.randn(n_comps, self.d_z))
+        self.prior_pi = nn.Parameter(torch.zeros(n_comps))
+
+    def sample_from_dataloader(self, data_loader):
+        all_data = []
+        bio_label = []
+        for batch in data_loader:
+            x = batch[0]
+            z = batch[2]  # biological variability
+            all_data.append(x)
+            bio_label.append(z)
+            if len(all_data) * x.shape[0] >= self.K:
+                break
+
+        all_data = torch.cat(all_data, dim=0)
+        bio_label = torch.cat(bio_label, dim=0)
+        bio_dict = torch.unique(bio_label, dim=0)
+
+        selected_u = []
+
+        for label in bio_dict:
+            for i in range(all_data.shape[0]):
+                if torch.equal(bio_label[i], label):
+                    selected_u.append(all_data[i])
+                    break
+
+        selected_u = torch.stack(selected_u)
+        return nn.Parameter(selected_u.clone().detach().requires_grad_(True))
+
+    def get_prior(self):
+        # encode pseudo inputs
+        mog_u = self.encoder(self.u)
+
+        # sample from encoded distribution to compute prior components centroids
+        mu_u = mog_u.rsample()
+
+        # compute prior
+        w = self.prior_pi.view(-1)
+        stds = torch.sqrt(torch.nn.functional.softplus(self.prior_var) + 1e-8)
+        prior = MixtureOfGaussians(w, mu_u, stds)
+
+        return prior
+
+    def sample(self, n_samples):
+
+        prior = self.get_prior()
+
+        return prior.rsample(sample_shape=torch.Size([n_samples]))
+
+    def pca_prior(self, n_samples):
+        """
+        Given a number of samples, get the PCA from the sampling of the prior distribution.
+        """
+        samples = self.sample(n_samples)
+        pca = PCA(n_components=2)
+        return pca.fit_transform(samples.detach().cpu())
+
+    def get_posterior(self, x):
+        """
+        Given a set of points, compute the posterior distribution.
+
+        Parameters:
+        x: [torch.Tensor]
+            Samples to pass to the encoder
+        """
+        q = self.encoder(x)
+        z = q.rsample()
+        return z
+
+    def pca_posterior(self, x):
+        """
+        Given a set of points, compute the PCA of the posterior distribution.
+
+        Parameters:
+        x: [torch.Tensor]
+            Samples to pass to the encoder
+        """
+        z = self.get_posterior(x)
+        pca = PCA(n_components=2)
+        return pca.fit_transform(z.detach().cpu())
+
+    def log_prob(self, z):
+
+        prior = self.get_prior()
+
+        return prior.log_prob(z)
+
+    def forward(self, x):
+        # Obtain posterior and sample
+        q_zx = self.encoder(x)
+        z = q_zx.rsample()
+
+        # Forward pass to the decoder
+        p_xz = self.decoder(z)
+
+        # Loss function
+        log_q_zx = q_zx.log_prob(z)
+        log_p_z = self.log_prob(z)
+
+        recon_term = p_xz.log_prob(x).mean()
+        kl_term = self.beta * (log_q_zx - log_p_z).mean()
 
         return -recon_term + kl_term
 
@@ -946,8 +1109,42 @@ class BatchDiscriminator(nn.Module):
         return loss(pred, true)
 
 
-# class BiologicalConservationClassifier(nn.Module):
-# return None
+class InputOutputDiscriminator(nn.Module):
+    """
+    Define a Discriminator that can tell decoder's output from input data apart.
+    """
+
+    def __init__(self, net):
+        super().__init__()
+        self.net = net
+
+    def forward(self, x):
+        pred = self.net(x)
+        return pred
+
+    def loss(self, pred, true):
+        loss = nn.CrossEntropyLoss()
+
+        return loss(pred, true)
+
+
+class BiologicalConservationClassifier(nn.Module):
+    """
+    Define a classifier in order to conserve biological relevant information.
+    """
+
+    def __init__(self, net):
+        super().__init__()
+        self.net = net
+
+    def forward(self, x):
+        pred = self.net(x)
+        return pred
+
+    def loss(self, pred, true):
+        loss = nn.CrossEntropyLoss()
+
+        return loss(pred, true)
 
 
 # ---------- TRAINING LOOP ---------- #
@@ -1008,11 +1205,14 @@ def train_abaco(
     discriminator,
     disc_optim,
     adv_optim,
+    bio_classifier,
+    bio_optim,
     data_loader,
     epochs,
     device,
     w_disc=1.0,
     w_adv=1.0,
+    w_bio=1.0,
     w_elbo=1.0,
     disc_loss_type="CrossEntropy",
 ):
@@ -1028,6 +1228,7 @@ def train_abaco(
         for loader_data in data_iter:
             x = loader_data[0].to(device)
             y = loader_data[1].to(device).float()
+            z = loader_data[2].to(device).float()
 
             # First step: Forward pass through encoder and to discriminator
             with torch.no_grad():
@@ -1066,7 +1267,20 @@ def train_abaco(
             # Detach elements
             z_adv = z_adv.detach()
 
-            # Third step: VAE ELBO computation
+            # Third step: Forward pass to biological classifier
+
+            bio_optim.zero_grad()
+            q_bio = vae.encoder(x)
+            z_bio = q_bio.rsample()
+            bio_pred_class = bio_classifier(z_bio)
+            bio_loss = w_bio * bio_classifier.loss(bio_pred_class, z)
+            bio_loss.backward()
+            bio_optim.step()
+
+            # Detach elements
+            z_bio = z_bio.detach()
+
+            # Forth step: VAE ELBO computation
             vae_optim.zero_grad()
             vae_loss = w_elbo * vae(x)
             vae_loss.backward()
@@ -1077,6 +1291,7 @@ def train_abaco(
                 vae_loss=f"{vae_loss.item():12.4f}",
                 disc_loss=f"{disc_loss.item():12.4f}",
                 adv_loss=f"{adv_loss.item():12.4f}",
+                bio_loss=f"{bio_loss.item():12.4f}",
                 epoch=f"{epoch+1}/{epochs}",
             )
             progress_bar.update()
